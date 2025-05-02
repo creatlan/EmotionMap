c1482463090c330aa271c78f536272cb77b03bc7
Danis Sharafiev <harnemer@gmail.com>
2025-05-02 20:45:08 +0300

    refactor(ml-service): refactored access to endpoints

--- backend/ml-service/config/config.py
+++ backend/ml-service/config/config.py
@@ -1,2 +1,16 @@
-MONGODB_SERVICE_HOST = "mongodb-service"
-MONGODB_SERVICE_PORT = 8001
+import os
+
+MONGODB_SERVICE_HOST = os.getenv("MONGODB_SERVICE_HOST", "mongodb-service")
+MONGODB_SERVICE_PORT = int(os.getenv("MONGODB_SERVICE_PORT", 8001))
+MONGODB_SERVICE_ENDPOINTS = {
+    "POINTS": "/points",
+    "USERS": "/users",
+    "CLUSTERS": "/clusters"
+}
+
+REDIS_SERVICE_HOST = os.getenv("REDIS_SERVICE_HOST", "redis-service")
+REDIS_SERVICE_PORT = int(os.getenv("REDIS_SERVICE_PORT", 8002))
+REDIS_SERVICE_ENDPOINTS = {
+    "VALUES": "/values",
+    "NUMBERS": "/numbers"
+}
